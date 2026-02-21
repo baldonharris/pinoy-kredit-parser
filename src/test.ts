13546d3fec1096a599e2da7f57dcfbfaf37db2f8
Harris Baldon <hbaldon93@gmail.com>
2026-02-21 15:06:58 +0800

    Delete test

--- src/test.ts
+++
@@ -1,8 +0,0 @@
-import { parseKredit } from './parseKredit'
-import { BankType } from './types'
-
-;(async () => {
-  const pdfPath = './statements/rcbc.pdf'
-  const transactions = await parseKredit(pdfPath, { bank: BankType.RCBC })
-  console.log(transactions)
-})()
